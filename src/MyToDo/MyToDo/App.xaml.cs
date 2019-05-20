940479c94da6de9a1daf6e3bd01ea3ae7d624b80
masjinno <masjinno@gmail.com>
2019-05-21 03:42:48 +0900

    refs #1 Add empty WPF project

---
+++ src/MyToDo/MyToDo/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MyToDo.View;
+using System.Windows;
+
+namespace MyToDo
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Main window
+        /// </summary>
+        private MainWindow view;
+
+        private void StartupApplication(object sender, StartupEventArgs e)
+        {
+            view = new MainWindow();
+            view.Show();
+        }
+    }
+}
